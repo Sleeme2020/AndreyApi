65b1280f72b78f3c04574136fc9fd2197ded50b3
Алексей <Alexsei07@bk.ru>
2024-05-11 19:00:56 +0300

    Исправлен метод получения и иэменения книги

--- AndreyApi/Controllers/BookControlers.cs
+++ AndreyApi/Controllers/BookControlers.cs
@@ -18,13 +18,13 @@ namespace AndreyApi.Controllers
         [HttpGet()]
         public ActionResult<object> Get()
         {
-            return new { Data = dbContext.Books.Include(u => u.Author).Include(u => u.Genre).Include(u => u.Image).Select(s => (BookUi)s).ToList() };
+            return new { Data = dbContext.Books.Include(u => u.Author).Include(u => u.Genre).Include(u => u.Image).Select(s =>(BookUi)s).ToList() };
         }
 
         [HttpGet("{Id:int}")]
         public ActionResult<BookUi> Get(int Id)
         {
-            var book = dbContext.Books.Include(u => u.Author).Include(u => u.Genre).FirstOrDefault(u => u.Id == Id);
+            var book = dbContext.Books.Include(u => u.Author).Include(u => u.Genre).Include(u => u.Image).FirstOrDefault(u => u.Id == Id);
             if (book == null) { return NotFound(); }
             return (BookUi)book;
         }
@@ -48,13 +48,14 @@ namespace AndreyApi.Controllers
         {
             if (book is null) { return BadRequest("Пустой автор"); }
             if (book.Title == string.Empty && book.Description == string.Empty
-                && book.AuthorId == 0 && book.GenreId == 0) { return BadRequest("Пустое имя"); }
+                && book.AuthorId == 0 && book.GenreId == 0 && book.ImageId == 0) { return BadRequest("Пустое имя"); }
             if (!dbContext.Books.Any(u => u.Id == Id)) { return BadRequest("Попытка добавления, воспользуйтесь методом POST"); }
             var aut = dbContext.Books.FirstOrDefault(u => u.Id == Id);
             aut.Title = book.Title;
             aut.Description = book.Description;
             aut.AuthorId = book.AuthorId;
             aut.GenreId = book.GenreId;
+            aut.ImageId = book.ImageId;
             dbContext.Books.Update(aut);
             dbContext.SaveChanges();
 
